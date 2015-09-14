e01343e616faa44b2ef045b05cf31e8bb4e8edeb
fffonion <fffonion@gmail.com>
2015-09-14 08:59:47 -0400

    fix #11

--- swjsq.py
+++ swjsq.py
@@ -278,7 +278,6 @@ portal=`$HTTP_REQ http://api.portal.swjsq.vip.xunlei.com:81/v2/queryportal`
 portal_ip=`echo $portal|grep -oE '([0-9]{1,3}[\.]){3}[0-9]{1,3}'`
 portal_port_temp=`echo $portal|grep -oE "port...[0-9]{1,5}"`
 portal_port=`echo $portal_port_temp|grep -oE '[0-9]{1,5}'`
-api_url="http://$portal_ip:$portal_port/v2"
 if [ -z "$portal_ip" ]
   then
 	 sleep 30
@@ -292,6 +291,7 @@ if [ -z "$portal_ip" ]
 	         portal_port=80
 	 fi
 fi
+api_url="http://$portal_ip:$portal_port/v2"
 i=6
 while true
 do
